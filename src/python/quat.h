8366714c4b65ca0b391c71449800563017ee8daf
Daniel Fiser <danfis@danfis.cz>
2011-01-03 16:23:37 +0100

    Added license and copyright notes to some files.

--- src/python/quat.h
+++ src/python/quat.h
@@ -1,3 +1,19 @@
+/***
+ * fermat
+ * -------
+ * Copyright (c)2010 Daniel Fiser <danfis@danfis.cz>
+ *
+ *  This file is part of fermat.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.php>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
 #ifndef PY_QUAT_H_
 #define PY_QUAT_H_
 
