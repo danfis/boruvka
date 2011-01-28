2690aa40afc14216dcddeabb09daa00e9edd3c67
Daniel Fiser <danfis@danfis.cz>
2011-01-28 15:37:28 +0100

    Updated license and copyright notes.

--- src/mesh/mesh3.c
+++ src/mesh/mesh3.c
@@ -1,3 +1,19 @@
+/***
+ * fermat
+ * -------
+ * Copyright (c)2011 Daniel Fiser <danfis@danfis.cz>
+ *
+ *  This file is part of fermat.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.php>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
 #include <fermat/mesh/mesh3.h>
 #include <fermat/alloc.h>
 #include <fermat/dbg.h>
