110a37574bb1da16771baffc307a3dacafda0faa
Daniel Fiser <danfis@danfis.cz>
2011-02-10 15:01:33 +0100

    Implemented PairHeap, without tests.

---
+++ fermat/fermat/pairheap.h
@@ -0,0 +1,171 @@
+/***
+ * fermat
+ * -------
+ * Copyright (c)2011 Daniel Fiser <danfis@danfis.cz>
+ *
+ *  This file is part of fermat.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.php>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
+#ifndef __FER_PAIRHEAP_H__
+#define __FER_PAIRHEAP_H__
+
+#include <fermat/core.h>
+#include <fermat/list.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * PairHeap - Pairing Heap
+ * ========================
+ *
+ * Fredman, Michael L.; Sedgewick, Robert; Sleator, Daniel D.; Tarjan,
+ * Robert E. (1986), "The pairing heap: a new form of self-adjusting heap",
+ * Algorithmica 1 (1): 111–129.
+ *
+ */
+
+struct _fer_pairheap_node_t {
+    fer_list_t children;             /*!< List of children */
+    fer_list_t list;                 /*!< Connection into list of siblings */
+};
+typedef struct _fer_pairheap_node_t fer_pairheap_node_t;
+
+/**
+ * Callback that should return true if {n1} is smaller than {n2}.
+ */
+typedef int (*fer_pairheap_lt)(const fer_pairheap_node_t *n1, const fer_pairheap_node_t *n2);
+
+struct _fer_pairheap_t {
+    fer_list_t root; /*!< List of root nodes. In fact, pairing heap has
+                          always one root node, but we need this to make
+                          effecient mergin.
+                          TODO: think about lazy merging */
+    fer_pairheap_lt lt; /*!< "Less than" callback provided by user */
+};
+typedef struct _fer_pairheap_t fer_pairheap_t;
+
+
+/**
+ * Creates new empty Pairing heap.
+ * Callback for comparison must be provided.
+ */
+fer_pairheap_t *ferPairHeapNew(fer_pairheap_lt less_than);
+
+/**
+ * Deletes pairing heap.
+ * Note that individual nodes are not disconnected from heap.
+ */
+void ferPairHeapDel(fer_pairheap_t *ph);
+
+/**
+ * Returns true if heap is empty.
+ */
+_fer_inline int ferPairHeapEmpty(const fer_pairheap_t *ph);
+
+/**
+ * Returns minimal node.
+ */
+_fer_inline fer_pairheap_node_t *ferPairHeapMin(fer_pairheap_t *ph);
+
+/**
+ * Adds node to heap.
+ */
+_fer_inline void ferPairHeapAdd(fer_pairheap_t *ph, fer_pairheap_node_t *n);
+
+/**
+ * Removes and returns minimal node from heap.
+ */
+_fer_inline fer_pairheap_node_t *ferPairHeapExtractMin(fer_pairheap_t *ph);
+
+/**
+ * Update position of node in heap in case its value was decreased.
+ * If value wasn't decreased (or you are not sure) call ferPairHeapUpdate()
+ * instead.
+ */
+_fer_inline void ferPairHeapDecreaseKey(fer_pairheap_t *ph, fer_pairheap_node_t *n);
+
+/**
+ * Updates position of node in heap.
+ */
+_fer_inline void ferPairHeapUpdate(fer_pairheap_t *ph, fer_pairheap_node_t *n);
+
+/**
+ * Del node from heap.
+ */
+void ferPairHeapRemove(fer_pairheap_t *ph, fer_pairheap_node_t *n);
+
+
+/**
+ * Consolidates pairing heap.
+ */
+void __ferPairHeapConsolidate(fer_pairheap_t *ph);
+
+
+/**** INLINES ****/
+_fer_inline int ferPairHeapEmpty(const fer_pairheap_t *ph)
+{
+    return ferListEmpty(&ph->root);
+}
+
+_fer_inline fer_pairheap_node_t *ferPairHeapMin(fer_pairheap_t *ph)
+{
+    fer_pairheap_node_t *el;
+    fer_list_t *item;
+
+    if (ferPairHeapEmpty(ph))
+        return NULL;
+
+    item = ferListNext(&ph->root);
+    el = ferListEntry(item, fer_pairheap_node_t, list);
+    return el;
+}
+
+_fer_inline void ferPairHeapAdd(fer_pairheap_t *ph, fer_pairheap_node_t *n)
+{
+    ferListInit(&n->children);
+    ferListAppend(&ph->root, &n->list);
+
+    __ferPairHeapConsolidate(ph);
+}
+
+_fer_inline void ferPairHeapDecreaseKey(fer_pairheap_t *ph, fer_pairheap_node_t *n)
+{
+    ferListDel(&n->list);
+    ferListAppend(&ph->root, &n->list);
+
+    __ferPairHeapConsolidate(ph);
+}
+
+_fer_inline void ferPairHeapUpdate(fer_pairheap_t *ph, fer_pairheap_node_t *n)
+{
+    ferPairHeapRemove(ph, n);
+    ferPairHeapAdd(ph, n);
+}
+
+_fer_inline fer_pairheap_node_t *ferPairHeapExtractMin(fer_pairheap_t *ph)
+{
+    fer_pairheap_node_t *n;
+
+    n = ferPairHeapMin(ph);
+    ferPairHeapRemove(ph, n);
+
+    return n;
+}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* __FER_PAIRHEAP_H__ */
+
+
---
+++ fermat/src/pairheap.c
@@ -0,0 +1,99 @@
+/***
+ * fermat
+ * -------
+ * Copyright (c)2011 Daniel Fiser <danfis@danfis.cz>
+ *
+ *  This file is part of fermat.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.php>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
+#include <fermat/pairheap.h>
+#include <fermat/alloc.h>
+
+fer_pairheap_t *ferPairHeapNew(fer_pairheap_lt less_than)
+{
+    fer_pairheap_t *ph;
+
+    ph = FER_ALLOC(fer_pairheap_t);
+    ferListInit(&ph->root);
+    ph->lt = less_than;
+
+    return ph;
+}
+
+void ferPairHeapDel(fer_pairheap_t *ph)
+{
+    free(ph);
+}
+
+void ferPairHeapRemove(fer_pairheap_t *ph, fer_pairheap_node_t *n)
+{
+    fer_pairheap_node_t *c;
+    fer_list_t *list, *item, *item_tmp;
+
+    list = &n->children;
+    ferListForEachSafe(list, item, item_tmp){
+        c = ferListEntry(item, fer_pairheap_node_t, list);
+
+        // remove from n
+        ferListDel(&c->list);
+        // add it to root list
+        ferListAppend(&ph->root, &c->list);
+    }
+
+    __ferPairHeapConsolidate(ph);
+}
+
+void __ferPairHeapConsolidate(fer_pairheap_t *ph)
+{
+    fer_list_t *root, *item, *item_next;
+    fer_pairheap_node_t *n1, *n2;
+
+    root = &ph->root;
+
+    // 1. First pairing from left to righ
+    item = ferListNext(root);
+    item_next = ferListNext(item);
+    while (item != root && item_next != root){
+        // get nodes
+        n1 = ferListEntry(item, fer_pairheap_node_t, list);
+        n2 = ferListEntry(item_next, fer_pairheap_node_t, list);
+
+        // compare them
+        if (ph->lt(n1, n2)){ // n1 < n2
+            ferListAppend(&n1->children, &n2->list);
+            item = ferListNext(&n1->list);
+        }else{
+            ferListAppend(&n2->children, &n1->list);
+            item = ferListNext(&n2->list);
+        }
+
+        item_next = ferListNext(item);
+    }
+
+    // 2. Finish mergin from right to left
+    // To be honest, I really don't understand should it be from right to
+    // left, so let's do it ordinary way...
+    item = ferListNext(root);
+    item_next = ferListNext(item);
+    while (item != root && item_next != root){
+        // get nodes
+        n1 = ferListEntry(item, fer_pairheap_node_t, list);
+        n2 = ferListEntry(item_next, fer_pairheap_node_t, list);
+
+        if (ph->lt(n1, n2)){ // n1 < n2
+            ferListAppend(&n1->children, &n2->list);
+        }else{
+            ferListAppend(&n2->children, &n1->list);
+            item = item_next;
+        }
+        item_next = ferListNext(item);
+    }
+}
