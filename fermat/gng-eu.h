c9ca13955dd0206cbd9b2d756b6db9ec128460f6
Daniel Fiser <danfis@danfis.cz>
2011-07-04 11:04:37 +0200

    GNG for euclidean metric.

---
+++ fermat/gng-eu.h
@@ -0,0 +1,210 @@
+/***
+ * fermat
+ * -------
+ * Copyright (c)2011 Daniel Fiser <danfis@danfis.cz>
+ *
+ *  This file is part of fermat.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.php>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
+#ifndef __FER_GNG_EU_H__
+#define __FER_GNG_EU_H__
+
+#include <fermat/vec.h>
+#include <fermat/pc.h>
+#include <fermat/nncells.h>
+#include <fermat/gng.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * Growing Neural Gas In Euclidean Metric
+ * =======================================
+ */
+
+
+
+/**
+ * GNG-Eu Operations
+ * --------------------
+ *
+ */
+
+/**
+ * Initialzes operations struct.
+ * All operations except terminate and callback are set.
+ */
+void ferGNGEuOpsInit(fer_gng_ops_t *ops);
+
+
+
+/**
+ * GNG-Eu Parameters
+ * -------------------
+ */
+struct _fer_gng_eu_params_t {
+    int dim; /*!< Number of dimensions. Default: 2 */
+
+    fer_gng_params_t gng;
+
+    int use_cells; /*!< True if cells should be used for nearest neighbor
+                        search. Default: true */
+    fer_nncells_params_t cells;
+};
+typedef struct _fer_gng_eu_params_t fer_gng_eu_params_t;
+
+/**
+ * Intializes parameters.
+ */
+void ferGNGEuParamsInit(fer_gng_eu_params_t *p);
+
+
+/**
+ * GNG-Eu Algorithm
+ * -----------------
+ */
+struct _fer_gng_eu_t {
+    int dim;
+
+    fer_gng_t *gng;   /*!< Pointer to GNG structure */
+    fer_pc_t *pc;     /*!< Point cloud of input signals */
+    fer_pc_it_t pcit; /*!< Iterator over .pc */
+
+    fer_nncells_t *cells; /*!< Cells for nearest neighbor search */
+};
+typedef struct _fer_gng_eu_t fer_gng_eu_t;
+
+
+/**
+ * Creates new instance of GNGEu algorithm.
+ */
+fer_gng_eu_t *ferGNGEuNew(const fer_gng_ops_t *ops,
+                          const fer_gng_eu_params_t *params);
+
+/**
+ * Deletes GNGEu.
+ */
+void ferGNGEuDel(fer_gng_eu_t *gng);
+
+/**
+ * Runs GNGEu algorithm.
+ */
+void ferGNGEuRun(fer_gng_eu_t *gng);
+
+/**
+ * Adds one input signal.
+ */
+_fer_inline void ferGNGEuAddInputSignal(fer_gng_eu_t *gng, const fer_vec_t *is);
+
+/**
+ * Adds input signals from file.
+ * Returns number of added input signals.
+ */
+_fer_inline size_t ferGNGEuAddInputSignalsFromFile(fer_gng_eu_t *gng,
+                                                   const char *fn);
+
+/**
+ * Returns point cloud.
+ */
+_fer_inline fer_pc_t *ferGNGEuPC(fer_gng_eu_t *gng);
+
+/**
+ * Returns GNG structure.
+ */
+_fer_inline fer_gng_t *ferGNGEuGNG(fer_gng_eu_t *gng);
+
+
+/**
+ * Net Related API
+ * ----------------
+ */
+struct _fer_gng_eu_node_t {
+    fer_gng_node_t node;    /*!< GNG node */
+    fer_vec_t *w;           /*!< Weight vector */
+    fer_nncells_el_t cells; /*!< Struct for searching in cubes2 */
+
+    int _id; /*!< Currently useful only for ferGNGEuDumpSVT(). */
+};
+typedef struct _fer_gng_eu_node_t fer_gng_eu_node_t;
+
+/**
+ * Returns number of nodes in net.
+ */
+_fer_inline size_t ferGNGEuNodesLen(const fer_gng_eu_t *gng);
+
+/**
+ * Cast from Net node to GNGEu node.
+ */
+_fer_inline fer_gng_eu_node_t *ferGNGEuNodeFromNet(fer_net_node_t *n);
+
+/**
+ * Cast from GNG node to GNGEu node.
+ */
+_fer_inline fer_gng_eu_node_t *ferGNGEuNodeFromGNG(fer_gng_node_t *n);
+
+
+/**
+ * Dumps 2-D network in SVT format.
+ */
+void ferGNGEuDumpSVT(fer_gng_eu_t *gng, FILE *out, const char *name);
+
+/**** INLINES ****/
+_fer_inline void ferGNGEuAddInputSignal(fer_gng_eu_t *gng, const fer_vec_t *is)
+{
+    ferPCAdd(gng->pc, is);
+}
+
+_fer_inline size_t ferGNGEuAddInputSignalsFromFile(fer_gng_eu_t *gng,
+                                                   const char *fn)
+{
+    return ferPCAddFromFile(gng->pc, fn);
+}
+
+_fer_inline fer_pc_t *ferGNGEuPC(fer_gng_eu_t *gng)
+{
+    return gng->pc;
+}
+
+_fer_inline fer_gng_t *ferGNGEuGNG(fer_gng_eu_t *gng)
+{
+    return gng->gng;
+}
+
+_fer_inline fer_net_t *ferGNGEuNet(fer_gng_eu_t *gng)
+{
+    return ferGNGNet(gng->gng);
+}
+
+_fer_inline size_t ferGNGEuNodesLen(const fer_gng_eu_t *gng)
+{
+    return ferGNGNodesLen(gng->gng);
+}
+
+_fer_inline fer_gng_eu_node_t *ferGNGEuNodeFromNet(fer_net_node_t *n)
+{
+    fer_gng_node_t *gn;
+    gn = ferGNGNodeFromNet(n);
+    return ferGNGEuNodeFromGNG(gn);
+}
+
+_fer_inline fer_gng_eu_node_t *ferGNGEuNodeFromGNG(fer_gng_node_t *n)
+{
+    return fer_container_of(n, fer_gng_eu_node_t, node);
+}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* __FER_GNG_EU_H__ */
+
+
---
+++ src/gng-eu.c
@@ -0,0 +1,328 @@
+/***
+ * fermat
+ * -------
+ * Copyright (c)2011 Daniel Fiser <danfis@danfis.cz>
+ *
+ *  This file is part of fermat.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.php>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
+#include <stdio.h>
+#include <fermat/gng-eu.h>
+#include <fermat/nearest-linear.h>
+#include <fermat/alloc.h>
+#include <fermat/dbg.h>
+
+/** Operations for fer_gng_ops_t struct */
+static fer_gng_node_t *ferGNGEuNewNode(const void *input_signal, void *);
+static fer_gng_node_t *ferGNGEuNewNodeBetween(const fer_gng_node_t *n1,
+                                              const fer_gng_node_t *n2,
+                                              void *);
+static void ferGNGEuDelNode(fer_gng_node_t *n, void *);
+static const void *ferGNGEuInputSignal(void *);
+static void ferGNGEuNearest(const void *input_signal,
+                            fer_gng_node_t **n1,
+                            fer_gng_node_t **n2,
+                            void *);
+_fer_inline void ferGNGEuNearestLinear(const void *input_signal,
+                                       fer_gng_node_t **n1,
+                                       fer_gng_node_t **n2,
+                                       void *);
+_fer_inline void ferGNGEuNearestCells(const void *input_signal,
+                                      fer_gng_node_t **n1,
+                                      fer_gng_node_t **n2,
+                                      void *);
+
+static fer_real_t ferGNGEuDist2(const void *input_signal,
+                                const fer_gng_node_t *node,
+                                void *);
+
+static void ferGNGEuMoveTowards(fer_gng_node_t *node,
+                                const void *input_signal,
+                                fer_real_t fraction, void *);
+
+
+void ferGNGEuOpsInit(fer_gng_ops_t *ops)
+{
+    ferGNGOpsInit(ops);
+
+    ops->init             = NULL;
+    ops->new_node         = ferGNGEuNewNode;
+    ops->new_node_between = ferGNGEuNewNodeBetween;
+    ops->del_node         = ferGNGEuDelNode;
+    ops->input_signal     = ferGNGEuInputSignal;
+    ops->nearest          = ferGNGEuNearest;
+    ops->dist2            = ferGNGEuDist2;
+    ops->move_towards     = ferGNGEuMoveTowards;
+}
+
+void ferGNGEuParamsInit(fer_gng_eu_params_t *p)
+{
+    p->dim = 2;
+
+    ferGNGParamsInit(&p->gng);
+
+    p->use_cells = 1;
+    ferNNCellsParamsInit(&p->cells);
+}
+
+fer_gng_eu_t *ferGNGEuNew(const fer_gng_ops_t *_ops,
+                          const fer_gng_eu_params_t *params)
+{
+    fer_gng_eu_t *gng;
+    fer_gng_ops_t ops;
+    fer_nncells_params_t cells_params;
+
+    gng = FER_ALLOC(fer_gng_eu_t);
+
+    gng->dim = params->dim;
+
+    ops = *_ops;
+    ops.init_data             = (void *)gng;
+    ops.new_node_data         = (void *)gng;
+    ops.new_node_between_data = (void *)gng;
+    ops.del_node_data         = (void *)gng;
+    ops.input_signal_data     = (void *)gng;
+    ops.nearest_data          = (void *)gng;
+    ops.dist2_data            = (void *)gng;
+    ops.move_towards_data     = (void *)gng;
+    gng->gng = ferGNGNew(&ops, &params->gng);
+
+    gng->pc = ferPCNew(gng->dim);
+
+    gng->cells = NULL;
+    if (params->use_cells){
+        cells_params = params->cells;
+        cells_params.d = gng->dim;
+        gng->cells = ferNNCellsNew(&cells_params);
+    }
+
+    return gng;
+}
+
+
+void ferGNGEuDel(fer_gng_eu_t *gng)
+{
+    ferPCDel(gng->pc);
+    ferGNGDel(gng->gng);
+
+    if (gng->cells)
+        ferNNCellsDel(gng->cells);
+
+    free(gng);
+}
+
+void ferGNGEuRun(fer_gng_eu_t *gng)
+{
+    ferPCPermutate(gng->pc);
+    ferPCItInit(&gng->pcit, gng->pc);
+
+    ferGNGRun(gng->gng);
+}
+
+
+static fer_gng_node_t *ferGNGEuNewNode(const void *input_signal, void *data)
+{
+    fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+    fer_gng_eu_node_t *n;
+
+    n = FER_ALLOC(fer_gng_eu_node_t);
+    n->w = ferVecClone(gng->dim, (const fer_vec_t *)input_signal);
+
+    if (gng->cells){
+        ferNNCellsElInit(&n->cells, n->w);
+        ferNNCellsAdd(gng->cells, &n->cells);
+    }
+
+    return &n->node;
+}
+
+static fer_gng_node_t *ferGNGEuNewNodeBetween(const fer_gng_node_t *_n1,
+                                              const fer_gng_node_t *_n2,
+                                              void *data)
+{
+    fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+    fer_vec_t w;
+    fer_gng_eu_node_t *n1, *n2;
+
+    n1 = fer_container_of(_n1, fer_gng_eu_node_t, node);
+    n2 = fer_container_of(_n2, fer_gng_eu_node_t, node);
+
+    ferVecAdd2(gng->dim, &w, n1->w, n2->w);
+    ferVecScale(gng->dim, &w, FER_REAL(0.5));
+
+    return ferGNGEuNewNode(&w, data);
+}
+
+static void ferGNGEuDelNode(fer_gng_node_t *_n, void *data)
+{
+    fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+    fer_gng_eu_node_t *n;
+
+    n = fer_container_of(_n, fer_gng_eu_node_t, node);
+
+    if (gng->cells){
+        ferNNCellsRemove(gng->cells, &n->cells);
+    }
+
+    ferVecDel(n->w);
+
+    free(n);
+}
+
+static const void *ferGNGEuInputSignal(void *data)
+{
+    fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+    const fer_vec_t *v;
+
+    if (ferPCItEnd(&gng->pcit)){
+        ferPCPermutate(gng->pc);
+        ferPCItInit(&gng->pcit, gng->pc);
+    }
+
+    v = ferPCItGet(&gng->pcit);
+    ferPCItNext(&gng->pcit);
+
+    return v;
+}
+
+static fer_real_t dist22(int dim, void *is, fer_list_t *nlist)
+{
+    fer_gng_node_t *gn;
+    fer_gng_eu_node_t *n;
+
+    gn = ferGNGNodeFromList(nlist);
+    n = fer_container_of(gn, fer_gng_eu_node_t, node);
+    return ferVecDist2(dim, (const fer_vec_t *)is, n->w);
+}
+
+static void ferGNGEuNearest(const void *input_signal,
+                            fer_gng_node_t **n1,
+                            fer_gng_node_t **n2,
+                            void *data)
+{
+    fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+
+    if (gng->cells){
+        ferGNGEuNearestCells(input_signal, n1, n2, data);
+    }else{
+        ferGNGEuNearestLinear(input_signal, n1, n2, data);
+    }
+}
+
+_fer_inline void ferGNGEuNearestLinear(const void *input_signal,
+                                       fer_gng_node_t **n1,
+                                       fer_gng_node_t **n2,
+                                       void *data)
+{
+    //fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+    fer_list_t *ns[2];
+
+    ns[0] = ns[1] = NULL;
+    // TODO ferNearestLinear(ferGNGNodes(gng->gng), (void *)input_signal, dist22, ns, 2);
+
+    *n1 = ferGNGNodeFromList(ns[0]);
+    *n2 = ferGNGNodeFromList(ns[1]);
+}
+
+_fer_inline void ferGNGEuNearestCubes(const void *input_signal,
+                                      fer_gng_node_t **n1,
+                                      fer_gng_node_t **n2,
+                                      void *data)
+{
+    fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+    fer_nncells_el_t *els[2];
+    fer_gng_eu_node_t *n;
+
+    *n1 = *n2 = NULL;
+
+    ferNNCellsNearest(gng->cells, (const fer_vec_t *)input_signal, 2, els);
+
+    n = fer_container_of(els[0], fer_gng_eu_node_t, cells);
+    *n1 = &n->node;
+    n = fer_container_of(els[1], fer_gng_eu_node_t, cells);
+    *n2 = &n->node;
+}
+
+static fer_real_t ferGNGEuDist2(const void *input_signal,
+                                const fer_gng_node_t *node,
+                                void *data)
+{
+    fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+    fer_gng_eu_node_t *n;
+    n = fer_container_of(node, fer_gng_eu_node_t, node);
+    return ferVecDist2(gng->dim, (const fer_vec_t *)input_signal, n->w);
+}
+
+static void ferGNGEuMoveTowards(fer_gng_node_t *node,
+                                const void *input_signal,
+                                fer_real_t fraction, void *data)
+{
+    fer_gng_eu_t *gng = (fer_gng_eu_t *)data;
+    fer_gng_eu_node_t *n;
+    fer_vec_t move;
+
+    n = fer_container_of(node, fer_gng_eu_node_t, node);
+    ferVecSub2(gng->dim, &move, (const fer_vec_t *)input_signal, n->w);
+    ferVecScale(gng->dim, &move, fraction);
+    ferVecAdd(gng->dim, n->w, &move);
+
+    if (gng->cells){
+        ferNNCellsUpdate(gng->cells, &n->cells);
+    }
+}
+
+void ferGNGEuDumpSVT(fer_gng_eu_t *gng, FILE *out, const char *name)
+{
+    /* TODO 
+    fer_list_t *list, *item;
+    fer_net_node_t *nn;
+    fer_gng_node_t *gn;
+    fer_net_edge_t *e;
+    fer_gng_eu_node_t *n;
+    size_t i, id1, id2;
+
+    fprintf(out, "--------\n");
+
+    if (name)
+        fprintf(out, "Name: %s\n", name);
+
+    fprintf(out, "Points:\n");
+    list = ferGNGNodes(gng->gng);
+    i = 0;
+    FER_LIST_FOR_EACH(list, item){
+        gn = ferGNGNodeFromList(item);
+        n  = ferGNGEuNodeFromGNG(gn);
+
+        n->_id = i++;
+        ferVec2Print(n->w, out);
+        fprintf(out, "\n");
+    }
+
+
+    fprintf(out, "Edges:\n");
+    list = ferGNGEdges(gng->gng);
+    FER_LIST_FOR_EACH(list, item){
+        e = FER_LIST_ENTRY(item, fer_net_edge_t, list);
+
+        nn = ferNetEdgeNode(e, 0);
+        n  = ferGNGEuNodeFromNet(nn);
+        id1 = n->_id;
+
+        nn = ferNetEdgeNode(e, 1);
+        n  = ferGNGEuNodeFromNet(nn);
+        id2 = n->_id;
+        fprintf(out, "%d %d\n", (int)id1, (int)id2);
+    }
+
+    fprintf(out, "--------\n");
+    */
+}
+
