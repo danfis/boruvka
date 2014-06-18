54443dd040c557e498d12483f3da209e3dd9026c
Daniel Fiser <danfis@danfis.cz>
2014-06-18 13:44:27 +0200

    MultiMapInt: Untested implementation.

---
+++ boruvka/multimap_int.h
@@ -0,0 +1,135 @@
+/***
+ * Boruvka
+ * --------
+ * Copyright (c)2014 Daniel Fiser <danfis@danfis.cz>
+ *
+ *  This file is part of Boruvka.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.bhp>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
+#ifndef __BOR_MULTIMAP_INT_H__
+#define __BOR_MULTIMAP_INT_H__
+
+#include <boruvka/list.h>
+#include <boruvka/rbtree_int.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * Multi Map with Integer Key
+ * ===========================
+ *
+ * This structure can stored multiple nodes associated with the (possibly
+ * same) integer key.
+ */
+
+/**
+ * Connector to the multi-map.
+ */
+typedef bor_list_t bor_multimap_int_node_t;
+
+
+
+struct _bor_multimap_int_keynode_t {
+    bor_list_t nodes;
+    bor_rbtree_int_node_t node;
+};
+typedef struct _bor_multimap_int_keynode_t bor_multimap_int_keynode_t;
+
+struct _bor_multimap_int_t {
+    bor_rbtree_int_t *tree;
+    bor_multimap_int_keynode_t *pre_keynode; /*!< Preallocated key-node */
+};
+typedef struct _bor_multimap_int_t bor_multimap_int_t;
+
+/**
+ * Creates a new multimap.
+ */
+bor_multimap_int_t *borMultiMapIntNew(void);
+
+/**
+ * Deletes the multi-map.
+ */
+void borMultiMapIntDel(bor_multimap_int_t *mm);
+
+/**
+ * Returns true if the multimap is empty.
+ */
+_bor_inline int borMultiMapIntEmpty(const bor_multimap_int_t *mm);
+
+/**
+ * Inserts a new node into the multimap and associate it with the given
+ * key.
+ */
+void borMultiMapIntInsert(bor_multimap_int_t *mm,
+                          int key, bor_multimap_int_node_t *node);
+
+/**
+ * Removes a node from the map.
+ */
+void borMultiMapIntRemove(bor_multimap_int_t *mm,
+                          bor_multimap_int_node_t *node);
+
+/**
+ * Returns a list of values associated with the key, or NULL if there is no
+ * such a key. See BOR_MULTIMAP_INT_FOR_EACH() for more convinient
+ * way to iterate over all values.
+ */
+bor_list_t *borMultiMapIntFind(bor_multimap_int_t *mm, int key);
+
+/**
+ * Returns a list of values associated with the minimal key in map.
+ * See BOR_MULTIMAP_INT_MIN_FOR_EACH().
+ */
+bor_list_t *borMultiMapIntMin(bor_multimap_int_t *mm);
+
+/**
+ * Removes from the multimap and returns the node associated with the
+ * minimal key. This function returns the node in FIFO manner.
+ */
+bor_multimap_int_node_t *borMultiMapIntExtractMinNodeFifo(bor_multimap_int_t *mm);
+
+/**
+ * Removes from the multimap and returns the node associated with the
+ * minimal key. This function returns the node in LIFO manner.
+ */
+bor_multimap_int_node_t *borMultiMapIntExtractMinNodeLifo(bor_multimap_int_t *mm);
+
+/**
+ * Iterates over all values associated with the specified key.
+ * The argument {list} is used for storing list's head and in {node} will
+ * be available pointer to the bor_multimap_int_node_t.
+ */
+#define BOR_MULTIMAP_INT_FOR_EACH(multimap, key, list, node) \
+    if (((list) = borMultiMapIntFind((multimap), (key))) != NULL) \
+        BOR_LIST_FOR_EACH(list, node)
+
+/**
+ * Iterates over all values associated with the minimal key.
+ * The argument {list} is used for storing list's head and in {node} will
+ * be available pointer to the bor_multimap_int_node_t.
+ */
+#define BOR_MULTIMAP_INT_MIN_FOR_EACH(multimap, list, node) \
+    if (((list) = borMultiMapIntMin(multimap)) != NULL) \
+        BOR_LIST_FOR_EACH(list, node)
+
+/**** INLINES: ****/
+_bor_inline int borMultiMapIntEmpty(const bor_multimap_int_t *mm)
+{
+    return borRBTreeIntEmpty(mm->tree);
+}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* __BOR_MULTIMAP_INT_H__ */
---
+++ src/multimap_int.c
@@ -0,0 +1,136 @@
+/***
+ * Boruvka
+ * --------
+ * Copyright (c)2014 Daniel Fiser <danfis@danfis.cz>
+ *
+ *  This file is part of Boruvka.
+ *
+ *  Distributed under the OSI-approved BSD License (the "License");
+ *  see accompanying file BDS-LICENSE for details or see
+ *  <http://www.opensource.org/licenses/bsd-license.bhp>.
+ *
+ *  This software is distributed WITHOUT ANY WARRANTY; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the License for more information.
+ */
+
+#include "boruvka/alloc.h"
+#include "boruvka/multimap_int.h"
+
+bor_multimap_int_t *borMultiMapIntNew(void)
+{
+    bor_multimap_int_t *mm;
+
+    mm = BOR_ALLOC(bor_multimap_int_t);
+    mm->tree = borRBTreeIntNew();
+    mm->pre_keynode = BOR_ALLOC(bor_multimap_int_keynode_t);
+    borListInit(&mm->pre_keynode->nodes);
+    return mm;
+}
+
+void borMultiMapIntDel(bor_multimap_int_t *mm)
+{
+    if (mm->tree)
+        borRBTreeIntDel(mm->tree);
+    if (mm->pre_keynode)
+        BOR_FREE(mm->pre_keynode);
+    BOR_FREE(mm);
+}
+
+void borMultiMapIntInsert(bor_multimap_int_t *mm,
+                          int key, bor_multimap_int_node_t *node)
+{
+    bor_multimap_int_keynode_t *keynode;
+    bor_rbtree_int_node_t *kn;
+
+    kn = borRBTreeIntInsert(mm->tree, key, &mm->pre_keynode->node);
+    if (kn == NULL){
+        // A new key-node was inserted, so pre-allocate a next key-node and
+        // remember the old one.
+        keynode = mm->pre_keynode;
+
+        mm->pre_keynode = BOR_ALLOC(bor_multimap_int_keynode_t);
+        borListInit(&mm->pre_keynode->nodes);
+    }else{
+        // The key is already in tree, infer the main struct
+        keynode = bor_container_of(kn, bor_multimap_int_keynode_t, node);
+    }
+
+    // Add the node to the end of the key-node list
+    borListAppend(&keynode->nodes, node);
+}
+
+void borMultiMapIntRemove(bor_multimap_int_t *mm,
+                          bor_multimap_int_node_t *node)
+{
+    bor_multimap_int_keynode_t *keynode;
+    bor_list_t *node_list;
+
+    // Remember the next node in the list.
+    // Later, we can check this node whether it isn't the head of the list
+    // and if so (and it is empty) than the key-node can be removed.
+    node_list = borListNext(node);
+
+    // Disconnect node from the multimap key-node
+    borListDel(node);
+
+    // Check whether the list isn't empty.
+    // This is a bit tricky. In the node_list can or cannot be stored the
+    // head of the list, but if this node is pointing to itself we now that
+    // it is the head of the list and we also know it is empty (thus
+    // borListEmpty() function). And if it is the head of the list we can
+    // infere the main key-node structure from it.
+    if (borListEmpty(node_list)){
+        keynode = BOR_LIST_ENTRY(node_list, bor_multimap_int_keynode_t, nodes);
+        borRBTreeIntRemove(mm->tree, &keynode->node);
+        BOR_FREE(keynode);
+    }
+}
+
+bor_list_t *borMultiMapIntFind(bor_multimap_int_t *mm, int key)
+{
+    bor_multimap_int_keynode_t *keynode;
+    bor_rbtree_int_node_t *kn;
+
+    kn = borRBTreeIntFind(mm->tree, key);
+    if (kn == NULL)
+        return NULL;
+
+    keynode = bor_container_of(kn, bor_multimap_int_keynode_t, node);
+    return &keynode->nodes;
+}
+
+bor_list_t *borMultiMapIntMin(bor_multimap_int_t *mm)
+{
+    bor_multimap_int_keynode_t *keynode;
+    bor_rbtree_int_node_t *kn;
+
+    kn = borRBTreeIntMin(mm->tree);
+    if (kn == NULL)
+        return NULL;
+
+    keynode = bor_container_of(kn, bor_multimap_int_keynode_t, node);
+    return &keynode->nodes;
+}
+
+bor_multimap_int_node_t *borMultiMapIntExtractMinNodeFifo(bor_multimap_int_t *mm)
+{
+    bor_list_t *nodes;
+    bor_multimap_int_node_t *node;
+
+    nodes = borMultiMapIntMin(mm);
+    node = borListNext(nodes);
+    borMultiMapIntRemove(mm, node);
+    return node;
+}
+
+bor_multimap_int_node_t *borMultiMapIntExtractMinNodeLifo(bor_multimap_int_t *mm)
+{
+    bor_list_t *nodes;
+    bor_multimap_int_node_t *node;
+
+    nodes = borMultiMapIntMin(mm);
+    node = borListPrev(nodes);
+    borMultiMapIntRemove(mm, node);
+    return node;
+}
